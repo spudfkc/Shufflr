70d52f2dccfa022a62cc50292ff39e74ece2c35b
spudfkc <potatoz@gmail.com>
2015-03-09 22:26:39 -0700

    initial prototype app - it authenticates and creates playlists

--- app.go
+++ app.go
@@ -1,40 +1,53 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"github.com/spudfkc/gotify"
+	"log"
 	"net/http"
 )
 
 type handler struct{}
-type internalServerErrorHandler struct{}
+type authHandler struct{}
+type authCallbackHandler struct{}
 
-type MyResponse struct {
-	name string `json:"myname"`
+var auth *gotify.Oauth2Authenticator
+
+func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	auth = gotify.CreateAuthenticator(gotify.PLAYLIST_READ_PRIVATE, gotify.PLAYLIST_MODIFY_PRIVATE, gotify.PLAYLIST_MODIFY_PUBLIC)
+	authUrl := auth.GetAuthURL()
+	http.Redirect(w, r, authUrl, http.StatusTemporaryRedirect)
 }
 
-func (Mux) ServeHTTP(w *http.ResponseWriter, r *http.Request) {
-}
+func (h *authCallbackHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	auth.ParseToken(r)
+	gotify.AuthorizeClient(auth)
+	w.Header().Set("AmIAuthorized", "Yes")
 
-func (h *internalServerErrorHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusInternalServerError)
-	fmt.Fprintf(w, "error")
+	user := gotify.User{"spudfkc", "lol", "derp"}
+	p, err := gotify.CreatePlaylist("goshuffleplaylist", true, user)
+	if err != nil {
+		log.Println("Failed to create playlist", err)
+	}
+	log.Println("playlist", p)
+
+	w.WriteHeader(http.StatusOK)
 }
 
 func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-
-	enc := json.NewEncoder(w)
-	if err := enc.Encode(&MyResponse{}); nil != err {
-        fmt.Fprintf(w, format, ...)
-	}
 }
 
 func main() {
-	mux := http.NewServeMux()
-	mux.Handle("/", &handler{})
+	h := handler{}
+	a := authHandler{}
+	c := authCallbackHandler{}
+
 	http.HandleFunc("/", h.ServeHTTP)
-	http.ListenAndServe(":8081", mux)
+	http.HandleFunc("/auth", a.ServeHTTP)
+	http.HandleFunc("/auth/cb", c.ServeHTTP)
+
+	http.ListenAndServe(":8081", nil)
 	fmt.Println("Listening on 8081...")
 }
